6d7975612b217e55117ea0c35b386c37b90a4294
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2025-02-01 04:02:09 +0600

    Added Trending Pet Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-bg': "url('/images/background.jpg')",
+      },
+    },
   },
   plugins: [],
 };
